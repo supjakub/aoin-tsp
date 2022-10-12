d72c0836a3cc9716ec43eb0dbb6afe3eb589ef27
Jakub Superczyński <jakubsuper1@gmail.com>
2022-10-13 01:58:11 +0200

    Utworzono osobny moduł logger

---
+++ logger.py
@@ -0,0 +1,27 @@
+class Coordinates:
+    x = None
+    y = None
+
+    def __init__(self, line):
+        array = line.split()
+        self.x = array[1]
+        self.y = array[2]
+
+class Logger:
+
+    def get_cords_from_file():
+        file_name = input('Wpisz nazwe pliku: ')
+        file = open(file_name)
+        line = file.readline()
+        while 'NODE_COORD_SECTION' not in line:
+            line = file.readline()
+        size = 0
+        cities = []
+        line = file.readline()
+        cities = []
+        while 'EOF' not in line:
+            size = size + 1
+            cities.append(Coordinates(line))
+            line = file.readline()
+        print(cities[0].x)
+    
--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-class Coordinates:
-    x = None
-    y = None
-
-    def __init__(self, line):
-        array = line.split()
-        self.x = array[1]
-        self.y = array[2]
-
-file_name = input('Wpisz nazwe pliku: ')
-file = open(file_name)
-line = file.readline()
-while 'NODE_COORD_SECTION' not in line:
-    line = file.readline()
-size = 0
-cities = []
-line = file.readline()
-cities = []
-while 'EOF' not in line:
-    size = size + 1
-    cities.append(Coordinates(line))
-    line = file.readline()
+import logger
+logger = logger.Logger
+logger.get_cords_from_file()
